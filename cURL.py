18df8fe6019cde7e402ca20ff0dff5cfc02ab908
Jarrett Goh <Jarrettgoh.xz@gmail.com>
2021-07-05 21:59:23 +0800

    Update cURL.py

--- cURL.py
+++ cURL.py
@@ -1,28 +1,68 @@
 import argparse
 import os
+import msvcrt
 
 # To make into .exe
+# Usage: --<HTTP request type> <request_route>
+# Example for GET request
+# => --get /api/random
 
 parser = argparse.ArgumentParser(
     description='HTTP GET, POST, PUT and DELETE request using cURL')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-parser.add_argument('--sum', dest='accumulate',
-                    action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
+parser.add_argument('--get',
+                    type=str,
+                    help='GET request',
+                    )
+parser.add_argument('--post',
+                    type=str,
+                    help='POST request',
+                    )
+parser.add_argument('--put',
+                    type=str,
+                    help='PUT request',
+                    )
+parser.add_argument('--delete',
+                    type=str,
+                    help='DELETE request',
+                    )
 
 
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+# vars() to make it iterable
+args = vars(parser.parse_args())
 
-# print('\n\n--------------------------------------')
-# print('cURL executable written in Python\n')
-# print('Usage guide: \n \'get\' \'post\' \'put\' \'delete\' followed by request route\n')
-# print('Example usage: \n get /api/test\n')
 
-# req_route = input('')
-req_url = f'http://localhost:4000/api/user{req_route}'
+url = 'http://localhost:4000/api/user'
+req_types = ['get', 'post', 'put', 'delete']
+
+for req_type in req_types:
+    if args[req_type] == None:
+        continue
+
+    else:
+        http_request_type = req_type
+        req_url = f'{url}{args[req_type]}'
+
+
+print(http_request_type)
+print(req_url)
+
+data = {}
+
+input_bytes = msvcrt.getch()
+if(input_bytes == b'\x18'):
+    send_request = input('Confirm changes to data (Y/N)? ')
+
+
+def process_input_text(text):
+    if(text == str(b'\x18')):
+        print(text)
+
+
+if (http_request_type == 'post'):
+    print('{\n')
+
+    input_str = input('')
+    process_input_text(input_str)
 
 
 def execute_shell_command(shell_command):
@@ -46,11 +86,3 @@ def PUT():
 # To confirm curl command
 def DELETE():
     execute_shell_command(f'curl -x DELETE {req_url}')
-
-
-def GET_TEST():
-    execute_shell_command(f'curl https://google.com')
-
-
-if('get' in req_route):
-    print(GET_TEST())
